fb76b6a4e00b59b7ed6df91f7e2cb8c97acbd468
yuansamuelandres <yuandeslytherinandres@gmail.com>
2025-02-26 19:31:33 +0200

    Added the first PHP code notes set

---
+++ tutorial.php
@@ -0,0 +1,22 @@
+<?php
+    echo 'Hello World!';
+    ECHO 'Hi';
+    echo '<br>';
+    print gettype("hi");
+
+    echo nl2br("hi
+    d");
+
+    $name = "Yuan";
+    // Heredoc
+    echo <<<"HelloHere"
+    <br>Hello PHP
+    $$$ ... ``` \\\
+    my name is $name
+    HelloHere;
+    // Nowdoc
+    echo <<<'HelloNow'
+    <br>Hello PHP
+    $$$ ... ``` \\\
+    my name is $name
+    HelloNow;
