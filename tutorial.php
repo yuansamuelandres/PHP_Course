8fb722a52115c9c2990f546baa03bb6285964080
yuansamuelandres <yuandeslytherinandres@gmail.com>
2025-03-17 14:06:51 +0200

    Added the second set of PHP code notes (12th-17th) in addition to the variables practise (15th)

--- tutorial.php
+++ tutorial.php
@@ -1,24 +1,28 @@
-<!-- In the name of Allah  -->
+<!-- In the name of Allah -->
 
 <?php
-    echo 'Hello World!';
-    ECHO 'Hi';
+    $username = "Yuan";
+    $$username = "him";
+    echo $Yuan;  # him
+    echo "Hello  ${$username}";  # Deprecated since PHP 8.2
+    echo "Hello ". $$username;  # Alternative
+    $a = "name";
+    $Yuan = &$a;
     echo '<br>';
-    print gettype("hi");
+    echo $a;    # name
+    echo $Yuan; # name
+?>
 
-    echo nl2br("hi
-    d");
 
-    $name = "Yuan";
-    // Heredoc
-    echo <<<"HelloHere"
-    <br>Hello PHP
-    $$$ ... ``` \\\
-    my name is $name
-    HelloHere;
-    // Nowdoc
-    echo <<<'HelloNow'
-    <br>Hello PHP
-    $$$ ... ``` \\\
-    my name is $name
-    HelloNow;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Page | <?php echo $username?></title>
+</head>
+<body>
+    <div>Welcome <?php echo $username?></div>
+    <div>You scored 1000 points</div>
+</body>
+</html>
