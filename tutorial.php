038ceae66f54c0b1d096f9d297e7d91afcdd0c0d
yuansamuelandres <yuandeslytherinandres@gmail.com>
2025-04-09 23:34:35 +0200

    Added the third set of PHP code notes (18th-23rd).

--- tutorial.php
+++ tutorial.php
@@ -1,16 +1,13 @@
 <!-- In the name of Allah -->
 
 <?php
-    $username = "Yuan";
-    $$username = "him";
-    echo $Yuan;  # him
-    echo "Hello  ${$username}";  # Deprecated since PHP 8.2
-    echo "Hello ". $$username;  # Alternative
-    $a = "name";
-    $Yuan = &$a;
-    echo '<br>';
-    echo $a;    # name
-    echo $Yuan; # name
+    define("DB_NAME", "Yuan");
+    echo DB_NAME;
+    echo "<br>";
+    echo __LINE__;
+    echo "<br>";
+    var_dump(0 <=> 100);    // int(-1)
+    echo "<br>";
 ?>
 
 
@@ -22,7 +19,6 @@
     <title>PHP Page | <?php echo $username?></title>
 </head>
 <body>
-    <div>Welcome <?php echo $username?></div>
-    <div>You scored 1000 points</div>
+    
 </body>
 </html>
